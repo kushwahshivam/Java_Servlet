fa351b10457b7e01b7fe585614f60e42667248e1
kushwahshivam <shivamkushwah97522@gmail.com>
2025-04-26 16:03:41 +0530

    adding login page

---
+++ eclipse-workspace/RegisterLogin/src/main/java/sp/in/backend/login.java
@@ -0,0 +1,54 @@
+package sp.in.backend;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/loginForm")
+public class login extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		String myemail = req.getParameter("email1");
+		String mypass = req.getParameter("pass1");
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/yt_demo","root","root");
+			PreparedStatement ps = con.prepareStatement("select * from register where email=? and password=?");
+			ps.setString(1,myemail);
+			ps.setString(2,mypass);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				HttpSession session = req.getSession();
+				session.setAttribute("session_name", rs.getString("name"));
+				RequestDispatcher rd = req.getRequestDispatcher("/profile.jsp");
+				rd.include(req, resp);
+			}
+			else {
+				out.print("<h3 style='color:red'> email id and password didnt match <h3/>");
+				RequestDispatcher rd = req.getRequestDispatcher("/login.jsp");
+				rd.include(req, resp);
+			}
+			
+		}catch(Exception e) {
+			out.print("<h3 style='color:red'> " +e.getMessage()+ " <h3/>");
+			RequestDispatcher rd = req.getRequestDispatcher("/login.jsp");
+			rd.include(req, resp);
+			e.printStackTrace();
+		}
+	}
+}
